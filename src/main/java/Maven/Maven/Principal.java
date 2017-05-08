1ae2e1907f3167da233cb21ae463dd2681316576
xameyz <xamey@hotmail.fr>
2017-05-08 23:49:51 +0200

    correction + mise en relation fonctions

--- src/main/java/Maven/Maven/Principal.java
+++ src/main/java/Maven/Maven/Principal.java
@@ -39,7 +39,7 @@ public class Principal extends JFrame implements ActionListener {
 	private JButton buttonAdd = new JButton("Ajouter Image");
 	private JButton buttonExit = new JButton("Quitter");
 
-	private JTabbedPane panel_R = new JTabbedPane();
+	private JPanel panel_R = new JPanel();
 
 	private JPanel panel_Down = new JPanel();
 	private JScrollPane panel_L;
@@ -51,13 +51,6 @@ public class Principal extends JFrame implements ActionListener {
 		// map
 		current_map = new Map();
 		
-		/*current_map.getMap().getMainMap().addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent e) {
-				System.out.println("Clic sur la map");
-			}
-		}); pour marc */
-		
-		// Panel du bas avec des boutons
 		
 		this.panel_L = transformImgListIntoJScroll.method(current_map.getImgList(), current_map);
 
@@ -75,20 +68,9 @@ public class Principal extends JFrame implements ActionListener {
 		
 		// panel de droite
 
-		panel_R.setBackground(Color.lightGray);
-		panel_R.setPreferredSize(new Dimension(200, 300));
+		panel_R=TransformImgIntoJPanel.method(current_map);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		final JComboBox combo=new JComboBox();
-		combo.setModel(populate());
-		combo.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent arg0){
-				String name=((ImagesNText)combo.getSelectedItem()).getName();
-				JOptionPane.showMessageDialog(null, null);
-				
-			}
-		});
-		panel_R.add(combo);
 		
 		// frame
 		add(current_map.getMap(), BorderLayout.CENTER);
@@ -103,19 +85,22 @@ public class Principal extends JFrame implements ActionListener {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		
-	
-	}
-		
-	
-	private DefaultComboBoxModel populate(){
-		DefaultComboBoxModel model=new DefaultComboBoxModel();
-		model.addElement(new ImagesNText(new ImageIcon("C:\\images\\fond.jpg"),"fond"));
-		model.addElement(new ImagesNText(new ImageIcon("C:\\images\\fond1.jpg"),"fond1"));
-		model.addElement(new ImagesNText(new ImageIcon("C:\\images\\fond2.jpg"),"fond2"));
-		
-		return model;
-	}
 
+		current_map.getMap().getMainMap().addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				remove(panel_R);
+				panel_R=TransformImgIntoJPanel.method(current_map);
+				add(panel_R, BorderLayout.EAST);
+				revalidate();
+				repaint();
+				
+				
+			}
+		}); 
+		
+		
+	}
+		
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		//String name=((ImagesNText)combo.getSelectedItem()).getName();
@@ -163,46 +148,7 @@ public class Principal extends JFrame implements ActionListener {
 			}
 		});
 	}
-class ImagesTextRenderer extends JLabel implements ListCellRenderer{
 	
-
-	public Component getListCellRendererComponent(JList list, Object val, int index, boolean isSelected,boolean focused){
-		// TODO Auto-generated method stub
-		ImagesNText it=(ImagesNText) val;
-		setIcon(it.getImg());
-		setText(it.getName());
-		if(isSelected){
-			setBackground(list.getSelectionBackground());
-			setForeground(list.getSelectionForeground());
-		}else{
-			setBackground(list.getBackground());
-			setForeground(list.getForeground());
-		}
-		setFont(list.getFont());
-		return this;
-	}
-	}
-	
-}
-class ImagesNText{
-	private Icon img;
-	private String name;
-	public ImagesNText(Icon img,String name){
-		this.img=img;
-		this.name=name;
-	}
-	public Icon getImg() {
-		return img;
-	}
-	public void setImg(Icon img) {
-		this.img = img;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 }
 
 	
